1f2d9b2a9d3ecca5f669de858539c1848e662c27
Adjel van der Meer <eavdmeer@gmail.com>
2022-12-30 08:37:20 +0100

    Rewrote day1 to new style

---
+++ day01/index.js
@@ -0,0 +1,77 @@
+import * as fs from 'fs/promises';
+import makeDebug from 'debug';
+
+const debug = makeDebug('dayxx');
+
+if (process.argv[2])
+{
+  dayxx(process.argv[2]).then(console.log);
+}
+
+function countIncreases(data)
+{
+  return data
+    .map((v, i, a) => i > 0 ? v > a[i - 1] ? 1 : 0 : 0)
+    .reduce((a, v) => a + v, 0);
+}
+
+function window(data, size)
+{
+  const result = [];
+
+  for (let idx = 0; idx <= data.length - size; idx++)
+  {
+    result.push(data.slice(idx, idx + size).reduce((a, v) => a + v, 0));
+  }
+  return result;
+
+  /*
+  return data
+    .map((v, i, d) => d.slice(i, i + size))
+    .filter(v => v.size < size)
+    .map(v => v.reduce((a, v) => a + v, 0));
+  */
+}
+
+function solve1(data)
+{
+  return countIncreases(data);
+}
+
+function solve2(data)
+{
+  return countIncreases(window(data, 3));
+}
+
+export default async function dayxx(target)
+{
+  const start = Date.now();
+  debug('starting');
+
+  const buffer = await fs.readFile(target);
+
+  /* eslint-disable no-shadow */
+  const data = buffer
+    .toString()
+    .trim()
+    .split(/\s*\n\s*/)
+    .filter(v => v)
+    .map(v => parseInt(v, 10));
+  /* eslint-enable no-shadow */
+
+  debug('data', data);
+
+  const part1 = solve1(data);
+  if (target.includes('example') && part1 !== 7)
+  {
+    throw new Error(`Invalid part 1 solution: ${part1}. Expecting; 7`);
+  }
+
+  const part2 = solve2(data);
+  if (target.includes('example') && part2 !== 5)
+  {
+    throw new Error(`Invalid part 2 solution: ${part2}. Expecting; 5`);
+  }
+
+  return { day: 1, part1, part2, duration: Date.now() - start };
+}
